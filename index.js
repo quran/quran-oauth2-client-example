383e53a727202f17ca256516fc2f5d57307baf44
Muhammad Muhajir <muhajirframe@gmail.com>
2023-01-27 11:37:34 +0700

    basic styling

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const { AuthorizationCode } = require('simple-oauth2')
-const app = require('express')()
-const dotenv = require('dotenv');
+const { AuthorizationCode } = require("simple-oauth2");
+const path = require("path");
+const app = require("express")();
+const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 const BASE_PATH = process.env.BASE_PATH;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
 const createApplication = (cb) => {
-  const callbackUrl = BASE_PATH + '/callback'
+  const callbackUrl = BASE_PATH + "/callback";
   app.listen(PORT, (err) => {
-    if (err) return console.error(err)
-    console.log(`Express server listening at ${BASE_PATH}`)
+    if (err) return console.error(err);
+    console.log(`Express server listening at ${BASE_PATH}`);
     return cb({
       app,
       callbackUrl,
-    })
-  })
-}
+    });
+  });
+};
 
 createApplication(({ app, callbackUrl }) => {
   const client = new AuthorizationCode({
@@ -26,47 +31,47 @@ createApplication(({ app, callbackUrl }) => {
     },
     auth: {
       tokenHost: process.env.TOKEN_HOST,
-      tokenPath: '/oauth2/token',
-      authorizePath: '/oauth2/auth',
+      tokenPath: "/oauth2/token",
+      authorizePath: "/oauth2/auth",
     },
-  })
+  });
 
   // Authorization uri definition
   const authorizationUri = client.authorizeURL({
     redirect_uri: callbackUrl,
     scope: process.env.SCOPES,
-    state: 'veimvfgqexjicockrwsgcb333o3a',
-  })
+    state: "veimvfgqexjicockrwsgcb333o3a",
+  });
 
   // Initial page redirecting to Github
-  app.get('/auth', (req, res) => {
-    console.log(authorizationUri)
-    res.redirect(authorizationUri)
-  })
+  app.get("/auth", (req, res) => {
+    console.log(authorizationUri);
+    res.redirect(authorizationUri);
+  });
 
   // Callback service parsing the authorization token and asking for the access token
-  app.get('/callback', async (req, res) => {
-    const { code } = req.query
-    console.log(code, 'this is the code')
+  app.get("/callback", async (req, res) => {
+    const { code } = req.query;
+    console.log(code, "this is the code");
     const options = {
       code,
       redirect_uri: callbackUrl,
-    }
+    };
 
     try {
-      const data = await client.getToken(options)
-      console.log(data)
+      const data = await client.getToken(options);
+      console.log(data);
 
-      console.log('The resulting token: ', data.token)
+      console.log("The resulting token: ", data.token);
 
-      return res.status(200).json(data.token)
+      return res.status(200).json(data.token);
     } catch (error) {
-      console.error('Access Token Error', error)
-      return res.status(500).json('Authentication failed')
+      console.error("Access Token Error", error);
+      return res.status(500).json("Authentication failed");
     }
-  })
+  });
 
-  app.get('/', (req, res) => {
-    res.send('Hello<br><a href="/auth">Continue with Quran.com</a>')
-  })
-})
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
+});
